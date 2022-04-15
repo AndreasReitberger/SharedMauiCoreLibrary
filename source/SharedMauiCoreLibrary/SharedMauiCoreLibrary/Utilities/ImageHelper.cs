c236fe8fd0fd1991f0c5e48f5f1246915b0ec922
Andreas <andy.reitberger@gmail.com>
2022-04-15 10:02:44 +0200

    Added new helpers

---
+++ source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Utilities/ImageHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace AndreasReitberger.Shared.Core.Utilities
+{
+    public partial class ImageHelper
+    {
+        #region Methods
+
+        public static byte[] ToArray(Microsoft.Maui.Graphics.IImage image)
+        {
+            using MemoryStream memory = new();
+
+            image.Save(memory);
+            return memory.ToArray();
+        }     
+        #endregion
+    }
+}
---
+++ source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Utilities/JsonConvertHelper.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace AndreasReitberger.Shared.Core.Utilities
+{
+    public partial class JsonConvertHelper
+    {
+        #region Converts
+        public static T ToObject<T>(string serversSettings, T defaultValue = default, Action<Exception> OnError = null)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(serversSettings) ?? defaultValue;
+            }
+            catch (Exception exc)
+            {
+                OnError?.Invoke(exc);
+                return defaultValue;
+            }
+        }
+        public static string ToSettingsString<T>(T settingsObject, string defaultValue = default, Action<Exception> OnError = null)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(settingsObject) ?? defaultValue;
+            }
+            catch (Exception exc)
+            {
+                OnError?.Invoke(exc);
+                return defaultValue;
+            }
+        }
+        #endregion
+    }
+}
