a3df59917aabcf2af13910bdc3539e4a45a57bf0
Andreas <andy.reitberger@gmail.com>
2022-06-22 10:14:54 +0200

    Added global WatchApp stuff
    Added globally used WatchApp classes
    Prepared for first release

---
+++ source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Platforms/iOS/Models/WatchAppEvent.cs
@@ -0,0 +1,13 @@
+﻿using AndreasReitberger.Shared.Core.Enums;
+using AndreasReitberger.Shared.Core.EventLogger;
+
+namespace AndreasReitberger.Shared.Core.Models
+{
+    public class WatchAppEvent : AppEvent
+    {
+        #region Properties
+        public WatchAppDevice Device { get; set; } = WatchAppDevice.Unkown;
+        public bool SessionValid { get; set; } = false;
+        #endregion
+    }
+}
