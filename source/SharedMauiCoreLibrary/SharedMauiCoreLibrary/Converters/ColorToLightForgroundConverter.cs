38ff20064abdcb1137100ee0f4b4b5bc27a0d9e0
Andreas <andy.reitberger@gmail.com>
2022-10-23 12:08:10 +0200

    Minor update
    - Updated ColorToBlackWhiteConverter to allow passing owh White and Black colors
    - Added new converter to determine if a light foreground is needed

--- source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Converters/ColorToLightForgroundConverter.cs
+++ source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Converters/ColorToLightForgroundConverter.cs
@@ -2,21 +2,20 @@
 
 namespace AndreasReitberger.Shared.Core.Converters
 {
-    public sealed class ColorToBlackWhiteConverter : IValueConverter
+    /// <summary>
+    /// Returns true, if the passed Color needs a light foreground
+    /// </summary>
+    public sealed class ColorToLightForgroundConverter : IValueConverter
     {
-        public Color White { get; set; } = Colors.White;
-        public Color Black { get; set; } = Colors.Black;
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            Color opposite = Colors.Black;
             if (value is Color color)
             {
                 float mean = (color.Red + color.Green + color.Blue) / 3;
-                opposite = mean < 0.5 ?
-                    White : Black;
+                return mean < 0.5;
             }
-            return opposite;
+            return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
