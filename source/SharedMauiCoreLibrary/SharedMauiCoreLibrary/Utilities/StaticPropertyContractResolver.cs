15c9f58d3d6ed276507cc9c6fe3f05653a9573d7
Andreas <andy.reitberger@gmail.com>
2022-11-12 14:39:27 +0100

    Upgrade to .net 7

---
+++ source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Utilities/StaticPropertyContractResolver.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace AndreasReitberger.Shared.Core.Utilities
+{
+    // Source: https://stackoverflow.com/questions/30193871/how-to-serialize-static-properties-in-json-net-without-adding-jsonproperty-att
+    public class StaticPropertyContractResolver : DefaultContractResolver
+    {
+        protected override List<MemberInfo> GetSerializableMembers(Type objectType)
+        {
+            var baseMembers = base.GetSerializableMembers(objectType);
+
+            PropertyInfo[] staticMembers =
+                objectType.GetProperties(BindingFlags.Static | BindingFlags.Public);
+
+            baseMembers.AddRange(staticMembers);
+
+            return baseMembers;
+        }
+    }
+}
