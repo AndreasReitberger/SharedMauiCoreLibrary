7a5fa1707826883897250dc1be9077710f778d67
Andreas <andy.reitberger@gmail.com>
2022-11-16 12:33:45 +0100

    Added TutorialHelper
    - Added new TutorialHelper class
    - Updated TutorialStep item class
    - Bumped version to 1.0.7

---
+++ source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Utilities/TutorialHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace AndreasReitberger.Shared.Core.Utilities
+{
+    public static class TutorialHelper
+    {
+        #region Methods
+        public static string GetTutorialImageSourceString(AppTheme theme, string baseDirectory, int order, string fileType = "png", string prefix = "")
+        {
+            return theme switch
+            {
+                AppTheme.Dark => $"{baseDirectory}/{prefix}{DeviceInfo.Platform.ToString().ToLowerInvariant()}_dark_{order}.{fileType}",
+                _ => $"{baseDirectory}/{prefix}{DeviceInfo.Platform.ToString().ToLowerInvariant()}_light_{order}.{fileType}",
+            };
+        }
+        public static string GetTutorialImageSourceString(AppTheme theme, int order, string fileType = "png", string prefix = "")
+        {
+
+            return theme switch
+            {
+                AppTheme.Dark => $"{prefix}{DeviceInfo.Platform.ToString().ToLowerInvariant()}_dark_{order}.{fileType}",
+                _ => $"{prefix}{DeviceInfo.Platform.ToString().ToLowerInvariant()}_light_{order}.{fileType}",
+            };
+        }
+        #endregion
+    }
+}
