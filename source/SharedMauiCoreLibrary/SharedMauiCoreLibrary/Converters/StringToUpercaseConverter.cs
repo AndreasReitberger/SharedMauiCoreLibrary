143445b337f9e40b6f4f2f0f3eaee4a3f51b1497
Andreas <andy.reitberger@gmail.com>
2022-12-29 17:22:42 +0100

    Add .gitignore file and converters
    This commits adds a .gitignore file to improve readability. Also some new default converters have been added.

---
+++ source/SharedMauiCoreLibrary/SharedMauiCoreLibrary/Converters/StringToUpercaseConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace AndreasReitberger.Shared.Core.Converters
+{
+    public class StringToUpercaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string str)
+            {
+                return str?.ToUpper();
+            }
+            else return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
