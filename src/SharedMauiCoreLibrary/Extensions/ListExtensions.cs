19c9ceca9136c74134be8a69902d68dda3422e43
Andreas <andy.reitberger@gmail.com>
2023-06-12 20:10:34 +0200

    Added `List` extensions
    This commit adds a `List` extension to `Split` up lists.
    
    Fixed #57

---
+++ src/SharedMauiCoreLibrary/Extensions/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Xml;
+
+namespace AndreasReitberger.Shared.Core.Extensions
+{
+    public static class ListExtensions
+    {
+        public static IEnumerable<List<T>> Split<T>(this List<T> source, int size)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / size)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+    }
+}
