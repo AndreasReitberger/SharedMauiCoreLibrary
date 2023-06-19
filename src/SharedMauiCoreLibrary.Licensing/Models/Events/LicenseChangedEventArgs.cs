a98787bd98ecb511dd97a41fa695ac6b4bcf329c
Andreas <andy.reitberger@gmail.com>
2023-06-19 13:54:00 +0200

    Improved `LicenseManager`
    Fixed #59

---
+++ src/SharedMauiCoreLibrary.Licensing/Models/Events/LicenseChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace AndreasReitberger.Shared.Core.Licensing.Events
+{
+    public class LicenseChangedEventArgs : EventArgs
+    {
+        #region Properties
+        public string Message { get; set; }
+        public DateTimeOffset CheckDate { get; set; }
+        public string LicenseKey { get; set; }
+        public bool Valid { get; set; }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+        #endregion
+    }
+}
