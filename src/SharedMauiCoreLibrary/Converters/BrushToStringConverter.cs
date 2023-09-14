71ef67f7dbf8a2bb054deb2cb74e61e9b3865be5
Andreas <andy.reitberger@gmail.com>
2023-09-14 07:30:58 +0200

    Added `Brush` converters
    Fixed #84

---
+++ src/SharedMauiCoreLibrary/Converters/BrushToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace AndreasReitberger.Shared.Core.Converters
+{
+    public sealed class BrushToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SolidColorBrush color)
+            {
+                return color?.Color?.ToHex();
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
