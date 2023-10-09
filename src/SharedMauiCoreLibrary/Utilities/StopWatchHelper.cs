211ea52e83c5d877a7ca961bd9c44df1b6c47190
Andreas <andy.reitberger@gmail.com>
2023-10-09 13:36:15 +0200

    Update StopWatchHelper.cs
    Minor improvements to the `StopWatchHelper`

--- src/SharedMauiCoreLibrary/Utilities/StopWatchHelper.cs
+++ src/SharedMauiCoreLibrary/Utilities/StopWatchHelper.cs
@@ -7,48 +7,57 @@ namespace AndreasReitberger.Shared.Core.Utilities
     {
         #region Methods
 #nullable enable
-        public static void Start(IDispatcher dispatcher, ref Stopwatch stopwatch, string methodName, IEventManager? eventManager = null)
+        public static void Start(IDispatcher dispatcher, ref Stopwatch stopwatch, string methodName, IEventManager? eventManager = null, bool log = false)
         {
             stopwatch?.Start();
             string msg = $"Performance: Start => {methodName}: {DateTime.Now})";
             dispatcher?.Dispatch(() =>
             {
-                eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(Start)}" });
+                if (log) eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(Start)}" });
                 Debug.WriteLine(msg);
             });
         }
-        public static async Task StartAsync(IDispatcher dispatcher, Stopwatch stopwatch, string methodName, IEventManager? eventManager = null)
+        public static async Task StartAsync(IDispatcher dispatcher, Stopwatch stopwatch, string methodName, IEventManager? eventManager = null, bool log = false)
         {
             stopwatch?.Start();
             string msg = $"Performance: Start => {methodName}: {DateTime.Now})";
             await dispatcher.DispatchAsync(() =>
             {
-                eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(StartAsync)}" });
+                if(log) eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(StartAsync)}" });
                 Debug.WriteLine(msg);
             });
         }
 
-        public static void Stop(IDispatcher dispatcher, ref Stopwatch stopwatch, string methodName, IEventManager? eventManager = null)
+        public static void Stop(IDispatcher dispatcher, ref Stopwatch stopwatch, string methodName, IEventManager? eventManager = null, bool log = false)
         {
             stopwatch?.Start();
             string msg = $"Performance: Done => {methodName}: {DateTime.Now} (Duration: {stopwatch?.Elapsed})";
             dispatcher?.Dispatch(() =>
             {
-                eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(Stop)}" });
+                if (log) eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(Stop)}" });
                 Debug.WriteLine(msg);
             });
         }
 
-        public static async Task StopAsync(IDispatcher dispatcher, Stopwatch stopwatch, string methodName, IEventManager? eventManager = null)
+        public static async Task StopAsync(IDispatcher dispatcher, Stopwatch stopwatch, string methodName, IEventManager? eventManager = null, bool log = false)
         {
             stopwatch?.Start();
             string msg = $"Performance: Done => {methodName}: {DateTime.Now} (Duration: {stopwatch?.Elapsed})";
             await dispatcher.DispatchAsync(() =>
             {
-                eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(StopAsync)}" });
+                if (log) eventManager?.LogInfo(new EventLogger.AppInfoEvent() { Message = msg, SourceName = $"{nameof(StopWatchHelper)}.{nameof(StopAsync)}" });
                 Debug.WriteLine(msg);
             });
         }
+
+        public static async Task DoTaskAndStopTimeAsync(IDispatcher dispatcher, string methodName, Func<Task> func, IEventManager? eventManager = null, bool logStart = false)
+        {
+            Stopwatch? watch = new();
+            await StartAsync(dispatcher, watch, methodName, eventManager, logStart);
+            try { await func.Invoke(); } catch (Exception ex) { eventManager?.LogError(ex); }
+            await StopAsync(dispatcher, watch, methodName, eventManager);
+        }
+
 #nullable disable
         #endregion
     }
