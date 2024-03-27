9c42815975ecc1dd344759dbf97881efe82b0f3b
Andreas <andy.reitberger@gmail.com>
2024-03-27 14:53:59 +0100

    Added  a `FileSaver` helper class
    Fixed #145

--- src/SharedMauiCoreLibrary/Hosting/AppHostBuilderExtensions.cs
+++ src/SharedMauiCoreLibrary/Hosting/AppHostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using AndreasReitberger.Shared.Core.Interfaces;
 using AndreasReitberger.Shared.Core.Localization;
 using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Storage;
 
 namespace AndreasReitberger.Shared.Core.Hosting
 {
@@ -9,6 +10,7 @@ namespace AndreasReitberger.Shared.Core.Hosting
         public static MauiAppBuilder ConfigureCoreLibrary(this MauiAppBuilder builder)
         {
             builder.UseMauiCommunityToolkit();
+            builder.ConfigureFileServices();
             return builder;
         }
 
@@ -30,6 +32,13 @@ namespace AndreasReitberger.Shared.Core.Hosting
                 BaseFlagImageUri = flagImageBaseDir
             };
             builder.Services.AddSingleton<ILocalizationManager>(manager);
+            return builder;
+        }
+
+        public static MauiAppBuilder ConfigureFileServices(this MauiAppBuilder builder)
+        {
+            builder.Services.AddSingleton<IFileSaver>(FileSaver.Default);
+            builder.Services.AddSingleton<ILauncher>(Launcher.Default);
             return builder;
         }
 
