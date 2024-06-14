87bf7e5832af8979ec9656402768294ed1b95b3f
Andreas <andy.reitberger@gmail.com>
2024-06-14 19:30:36 +0200

    Added a `Contact` and `ChatMessage` common class
    Fixed #186

---
+++ src/SharedNetCoreLibrary/Models/Contacts/Contact.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AndreasReitberger.Shared.Core.Contacts
+{
+    public partial class Contact : ObservableObject
+    {
+        #region Properties
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Name))]
+        string firstName = string.Empty;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Name))]
+        string lastName = string.Empty;
+
+        [ObservableProperty]
+        DateTimeOffset? timeAdded;
+
+        [ObservableProperty]
+        Dictionary<string, string> phoneNumbers =[];
+
+        [ObservableProperty]
+        Dictionary<string, string> emailAddresses =[];
+
+        public string Name => string.IsNullOrEmpty(FirstName) ? $"{LastName}" : $"{LastName}, {FirstName}";
+        #endregion
+
+        #region Ctor
+        public Contact() { }
+        
+        #endregion
+    }
+}
