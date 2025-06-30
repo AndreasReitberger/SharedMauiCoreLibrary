e30244926b8be1a18f230bd10f8b4d46f8d2a5d9
Andreas <andy.reitberger@gmail.com>
2025-06-30 17:55:05 +0200

    Create PreserveAttribute.cs
    Added `PreservAttribute` to shared .NET library.
    
    Fixed #233

---
+++ src/SharedNetCoreLibrary/Attributes/PreserveAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace AndreasReitberger.Shared.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false)]
+    sealed class PreserveAttribute : Attribute
+    {
+        public bool AllMembers;
+        public bool Conditional;
+    }
+}
