be1b612ed790c06f00b1c352f79648ab0b2cd2f5
Andreas <andy.reitberger@gmail.com>
2025-10-27 10:37:55 +0100

    Create ColorToVariantConverter.cs
    PR is still in progress...
    
    Fixed #273

---
+++ src/SharedMauiCoreLibrary/Converters/ColorToVariantConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace AndreasReitberger.Shared.Core.Converters
+{
+    public sealed class ColorToVariantConverter : IValueConverter
+    {
+        public float Factor { get; set; } = 0.50f;
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            Color opposite = Colors.Black;
+            if (value is Color color)
+            {
+                float red = color.Red * Factor;
+                float green = color.Green * Factor;
+                float blue = color.Blue * Factor;
+                return new Color(red, green, blue, color.Alpha);
+            }
+            return opposite;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
