692dee228763f8081f9120b292935b74a50e30e2
Andreas <andy.reitberger@gmail.com>
2025-12-14 09:30:19 +0100

    Added `BooleanToIconConverter`
    Fixed #317

---
+++ src/SharedMauiCoreLibrary/Converters/BooleanToIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace AndreasReitberger.Shared.Core.Converters
+{
+    public sealed class BooleanToIconConverter : IValueConverter
+    {
+        public string True { get; set; } = string.Empty;
+        public string False { get; set; } = string.Empty;
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool trueState)
+            {
+                if (trueState)
+                    return True;
+                else 
+                    return False;
+            }
+            return False;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
