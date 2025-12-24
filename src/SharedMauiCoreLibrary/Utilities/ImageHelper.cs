0bd4e69a4e6ef37647ad1210a5e662e6fcf9ecb8
Andreas <andy.reitberger@gmail.com>
2025-12-24 19:01:56 +0100

    Improved `ImageHelper`

--- src/SharedMauiCoreLibrary/Utilities/ImageHelper.cs
+++ src/SharedMauiCoreLibrary/Utilities/ImageHelper.cs
@@ -10,19 +10,33 @@
             image.Save(memory);
             return memory.ToArray();
         }
-
-        public static byte[] ToArray(Image image)
+        public static byte[] ToArray(FileStream imageStream)
         {
-            // Based on https://github.com/CommunityToolkit/Maui/blob/main/src/CommunityToolkit.Maui/Converters/ByteArrayToImageSourceConverter.shared.cs
-            if (image?.Source is not StreamImageSource streamImageSource)
+            if (imageStream is null)
                 return [];
-
             using MemoryStream memory = new();
-            Stream? streamFromImageSource = streamImageSource.Stream(CancellationToken.None).GetAwaiter().GetResult();
-            if (streamFromImageSource is null)
-                return [];
-            streamFromImageSource.CopyTo(memory);
+            imageStream.CopyTo(memory);
             return memory.ToArray();
+        }
+        public static async Task<byte[]> ToArrayAsync(FileStream imageStream)
+        {
+            if (imageStream is null)
+                return [];
+            using MemoryStream memory = new();
+            await imageStream.CopyToAsync(memory);
+            return memory.ToArray();
+        }
+        public static Task<byte[]> ToArrayAsync(string imagePath) => ToArrayAsync(GetImageStream(imagePath));
+        public static FileStream GetImageStream(string filePath) => File.OpenRead(filePath);
+        public static Task<FileStream> GetImageStreamAsync(string filePath) => Task.Run(() => File.OpenRead(filePath));
+        public static async Task<ImageSource?> LoadImageFromStream(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                using FileStream imageStream = await GetImageStreamAsync(filePath).ConfigureAwait(false);
+                return ImageSource.FromStream(() => imageStream);
+            }
+            else return null;
         }
         #endregion
     }
