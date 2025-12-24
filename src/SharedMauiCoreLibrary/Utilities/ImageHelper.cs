c843ba01ccaca22eb7ad5df6e31a22cf5cfda4ad
Andreas <andy.reitberger@gmail.com>
2025-12-24 16:40:58 +0100

    Added `ToArray(Image image)` to `ImageHelper`
    Fixed #325

--- src/SharedMauiCoreLibrary/Utilities/ImageHelper.cs
+++ src/SharedMauiCoreLibrary/Utilities/ImageHelper.cs
@@ -7,8 +7,21 @@
         public static byte[] ToArray(Microsoft.Maui.Graphics.IImage image)
         {
             using MemoryStream memory = new();
-
             image.Save(memory);
+            return memory.ToArray();
+        }
+
+        public static byte[] ToArray(Image image)
+        {
+            // Based on https://github.com/CommunityToolkit/Maui/blob/main/src/CommunityToolkit.Maui/Converters/ByteArrayToImageSourceConverter.shared.cs
+            if (image?.Source is not StreamImageSource streamImageSource)
+                return [];
+
+            using MemoryStream memory = new();
+            Stream? streamFromImageSource = streamImageSource.Stream(CancellationToken.None).GetAwaiter().GetResult();
+            if (streamFromImageSource is null)
+                return [];
+            streamFromImageSource.CopyTo(memory);
             return memory.ToArray();
         }
         #endregion
