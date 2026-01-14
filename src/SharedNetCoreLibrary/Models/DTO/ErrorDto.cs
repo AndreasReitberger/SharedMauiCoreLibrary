85ff404057bde0ae181ec68a808dfe13df5a52ff
Andreas <andy.reitberger@gmail.com>
2026-01-14 10:04:56 +0100

    Added `ErrorDto` and the mapper
    Fixed #334

---
+++ src/SharedNetCoreLibrary/Models/DTO/ErrorDto.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AndreasReitberger.Shared.Core.Models.DTO
+{
+    public partial class ErrorDto : ObservableObject
+    {
+        #region Properties
+
+        [ObservableProperty]
+        public partial string Type { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial string Message { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial string? StackTrace { get; set; }
+
+        [ObservableProperty]
+        public partial ErrorDto? InnerError { get; set; }
+
+        #endregion
+
+        #region Ctor
+        public ErrorDto() { }
+        public ErrorDto(string type, string message, string? stackTrace, ErrorDto? innerError) : this()
+        {
+            Type = type;
+            Message = message;
+            StackTrace = stackTrace;
+            InnerError = innerError;
+        }
+
+        #endregion
+    }
+}
---
+++ src/SharedNetCoreLibrary/Utilities/DtoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AndreasReitberger.Shared.Core.Models.DTO;
+
+namespace AndreasReitberger.Shared.Core.Utilities
+{
+    public static class DtoMapper
+    {
+        public static ErrorDto? FromException(Exception? ex) =>
+                ex is null ? null :
+                new ErrorDto(
+                    ex.GetType().FullName ?? "System.Exception",
+                    ex.Message,
+                    ex.StackTrace,
+                    FromException(ex.InnerException));
+
+    }
+}
