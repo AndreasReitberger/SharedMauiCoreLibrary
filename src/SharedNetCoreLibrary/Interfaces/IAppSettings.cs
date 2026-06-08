d61e41bbfdcb46fe6f687a2cc3319b5f56621de8
Andreas <andy.reitberger@gmail.com>
2026-06-08 06:18:40 +0200

    Delete IAppSettings.cs
    Fixed #425

--- src/SharedNetCoreLibrary/Interfaces/IAppSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AndreasReitberger.Shared.Core.Interfaces
-{
-    public interface IAppSettings
-    {
-        public void Save();
-        public void Load();
-        public void Reset();
-    }
-}
