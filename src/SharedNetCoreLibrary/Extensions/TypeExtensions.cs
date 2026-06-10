0dabbac95e57cf549404da425df84eff3af09871
Andreas <andy.reitberger@gmail.com>
2026-06-10 11:29:35 +0200

    Added further extensions
    Fixed #438

---
+++ src/SharedNetCoreLibrary/Extensions/TypeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace AndreasReitberger.Shared.Core.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static bool IsAnonymousType(this Type type)
+        {
+            if (type == null)
+                return false;
+
+            return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false)
+                   && type.IsGenericType
+                   && type.Name.Contains("AnonymousType")
+                   && (type.Name.StartsWith("<>") || type.Name.StartsWith("VB$"))
+                   && (type.Attributes & TypeAttributes.NotPublic) == TypeAttributes.NotPublic;
+        }
+
+    }
+}
