17af9686d4806175d794094fe172d08220cad0a2
Andreas <andy.reitberger@gmail.com>
2026-06-12 07:24:09 +0200

    Removed inheritance of `JsonSerializerContext`
    This commit adds a new `JsonOptionsProvider` allowing to combine `JsonSerialzeContext`. However, this will require unsafe code.
    
    Fixed #449

---
+++ src/SharedMauiCoreLibrary.Test/SourceGeneration/TestContext.cs
@@ -0,0 +1,11 @@
+﻿using SharedMauiCoreLibrary.Test.Models;
+using System.Text.Json.Serialization;
+
+namespace SharedMauiCoreLibrary.Test.SourceGeneration
+{
+    [JsonSerializable(typeof(TestModel))]
+    [JsonSourceGenerationOptions(WriteIndented = true)]
+    public partial class TestContext : JsonSerializerContext
+    {
+    }
+}
