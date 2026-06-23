14ece38797732416bf28ef6dce68fcd2902e4445
Andreas <andy.reitberger@gmail.com>
2026-06-23 10:08:09 +0200

    Added further base classes
    Fixed #453

---
+++ src/SharedNetCoreLibrary/Models/Person/Person.cs
@@ -0,0 +1,25 @@
+﻿using AndreasReitberger.Shared.Core.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AndreasReitberger.Shared.Core.Person
+{
+    public partial class Person : ObservableObject, IPerson
+    {
+        #region Properties
+        [ObservableProperty]
+        public partial string Salutation { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial string FirstName { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial string LastName { get; set; } = string.Empty;
+
+        [ObservableProperty]
+        public partial IAddress? Address { get; set; }
+
+        [ObservableProperty]
+        public partial ICollection<IContactDetail> ContactDetails { get; set; } = [];
+        #endregion
+    }
+}
